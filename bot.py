4bb2a3e655cb1488a5206090c6b1bbe3b75754c8
ozizenko <ozizenko2@gmail.com>
2026-06-02 02:45:24 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -45,7 +45,7 @@ async def handle(update: Update, context: ContextTypes.DEFAULT_TYPE):
             payload="premium",
             provider_token="",
             currency="XTR",
-            prices=[LabeledPrice("Premium", 10000)]
+            prices=[LabeledPrice("Premium", 1)]
         )
         return
 
